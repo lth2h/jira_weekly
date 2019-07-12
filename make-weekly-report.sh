f1b32f986a9c925fd036b96b7efd6a7e736adebd
L T Harris <lth2h@yahoo.com>
2019-07-12 12:55:57 -0400

    Fix make-weekly-report to use correct options.

--- make-weekly-report.sh
+++ make-weekly-report.sh
@@ -1,6 +1,6 @@
 ./jira_epic_report.pl --done --no-jira > /tmp/output.html
 ./jira_epic_report.pl  > /tmp/output2.html
-./jira_weekly_report.pl --no-items > /tmp/output3.txt
+./jira_weekly_report.pl > /tmp/output3.txt
 
 
 
