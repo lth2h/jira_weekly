f6ce85f788429b0f73ef158ff28989df36f52a2c
L T Harris <lth2h@yahoo.com>
2019-07-12 12:27:52 -0400

    Fix weekly report to use API.  Removed unused features.

--- jira_weekly_report.pl
+++ jira_weekly_report.pl
@@ -1,19 +1,6 @@
 #!/usr/bin/perl
 
 #######
-# TODO: --no-sub
-#       --group-by-sub=done --group-by-sub=active
-# Change to use API token by June 3, 2019
-
-# Use an API token
-# A primary use case for API tokens is to allow scripts to access REST APIs for Atlassian Cloud applications using HTTP basic authentication.
-
-# Depending on the details of the HTTP library you use, simply replace your password with the token. For example, when using curl, you could do something like this:
-
-# curl -v https://mysite.atlassian.net --user me@example.com:my-api-token
-# Note that me@example.com here is the email address for the Atlassian account you're using to create the token.
-
-#######
 
 # use jira_archive.pl to archive and create the Weekly Report task.
 
@@ -22,38 +9,26 @@
 use strict;
 use warnings;
 
-# binmode STDOUT, ":utf8";
-# binmode STDOUT, ":encoding(C)";
-# use open ':encoding(C)';
-
 use LWP::UserAgent;
 use Data::Dumper;
 
-# use URI;
-use XML::Feed;
-use HTML::Strip;
-use Lingua::EN::Sentence qw(get_sentences);
 use Date::Calc qw(Delta_Days Add_Delta_Days);
-use JIRA::Client::Automated;
 use Sort::Naturally;
+use JIRA::REST::Class;
 
 use YAML;
-use File::Slurp; # is this actually needed???
-# use Test::YAML::Valid;
-# use Kwalify qw(validate);
+use File::Slurp qw(read_file);
 
 use Getopt::Long;
 use POSIX qw(strftime);
 
 my ($verbose, $debug, $dry, $quiet);
-my $show_entries;
 my $max_days;
 my $mdt = 1;
-my $ignore_level = 0;
-my ($no_items, $short_items, $no_done);
-my ($rss_file, $write_rss, $write_rss_only);
+my $no_done;
 my $yorn;
 my $fdate;
+my $yes;
 
 my $last_item = 0;
 
@@ -61,28 +36,12 @@ GetOptions(
 	   "verbose" => \$verbose,
 	   "quiet" => \$quiet,
 	   "debug" => \$debug,
-	   "show-entries" => \$show_entries,
 	   "days=i" => \$max_days,
-	   "ignore_weekly=i" => \$ignore_level,
-	   "no-items" => \$no_items,
-	   "short-items" => \$short_items,
 	   "no-export-done" => \$no_done,
-	   "use-rss-file=s" => \$rss_file,
 	   "dry-run" => \$dry,
-	   "write-rss!" => \$write_rss,
-	   "only-write-rss" => \$write_rss_only,
 	   "fdate=i" => \$fdate,
+	   "y" => \$yes,
 ) or usage();
-
-if (!defined($write_rss)) {
-
-  $write_rss = 1; # always write rss unless given --no-write-rss
-
-}
-
-if ($write_rss_only) {
-  $write_rss = 1;
-}
 
 # my $max_days = $ARGV[0] || 14;
 if (!defined($max_days)) {
@@ -93,8 +52,6 @@ if (!defined($max_days)) {
 
 }
 
-my $hs = HTML::Strip->new();
-
 my $yaml;
 $yaml = read_file("jira_weekly.yaml");
 
@@ -102,8 +59,8 @@ my %yh = %{Load($yaml)}; # the yaml hash
 
 print Dumper \%yh if $debug;
 
-my $username = $yh{"username"};
-my $password = $yh{"password"};
+my $username = $yh{"api_user"};
+my $password = $yh{"api_token"};
 my %staff = %{$yh{"staff"}};
 my @verbs = @{$yh{"verbs"}};
 my $filter = $yh{"filter"};
@@ -111,13 +68,12 @@ my $jira_domain = $yh{"jira_domain"};
 
 # because of https://ecosystem.atlassian.net/browse/STRM-2140 and other bugs, a date range needs to be applied. Date format is JavaScript's miliseconds since the Epoch.
 # THIS IS DIFFERENT FROM the --days=x option AND THE SHORTER OF THE TWO WILL CONTROL
-if (!$fdate) {$fdate = $yh{"fdate"}; }
-my $first_fdate = 1000*(time() - $fdate*24*60*60); #we'll try two months
-my $last_fdate = 1000*(time());
-my $date_filter="&streams=update-date+BETWEEN+$first_fdate+$last_fdate";
 
-my $url = "https://$username:$password\@$jira_domain/activity?maxResults=1000&streams=$filter$date_filter&os_authType=basic&title=Activity%20Stream";
-print $url . "\n" if $debug;
+my $jira2 = JIRA::REST::Class->new({
+    url             => "https://$jira_domain",
+    username        => $username,
+    password        => $password,
+});
 
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my $now_string = strftime("%Y-%m-%d_%H%M%S", ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst));
@@ -127,39 +83,18 @@ $mon+=1;
 my $lzmon = sprintf('%02d', $mon);
 my $lzmday = sprintf('%02d', $mday);
 
-if (($write_rss) && (!$rss_file))  {
-  # write rss but not if we've given it an rs file
-
-  my $ua = LWP::UserAgent->new;
-  my $response = $ua->get($url);
-
-  my $dc = $response->decoded_content;  # this needs to go into a file for XML::RSS::Parser
-
-  print "RSS FILE: $rss_file\n" if (($debug) && ($rss_file));
-  $rss_file = "/tmp/$now_string-jira_dc.rss" unless $rss_file;
-  print "RSS FILE IS NOW: $rss_file\n" if (($debug) && ($rss_file));
-
-  open (FILE, "+>$rss_file") or die("kaboom: $rss_file $!");
-
-  print FILE $dc;
-
-  close (FILE);
-
-  print "RSS file written to $rss_file\n" unless $quiet;
-
-  if ($write_rss_only) { exit; }
-
-}
-
-# print Dumper \$dc;
-
-
 ## check last report
+
+my $last_day;
+my $last_mo;
+my $last_yr;
+my $last_key;
+my $last_summary;
+
 if ( -e "./jira_get_last_report_date.pl") {
 
   # get last report date from the summary only instead of both summary and highest key
   # maybe this should be an option
-  # my $last_report_yaml = `./jira_get_last_report_date.pl`;
   my $last_report_yaml = `./jira_get_last_report_date.pl --summary-only`;
 
   if (!ck_exit_status($?)) {
@@ -176,11 +111,11 @@ if ( -e "./jira_get_last_report_date.pl") {
 
   # print Dumper \%lr_yh;
 
-  my $last_day = $lr_yh{"max_day"};
-  my $last_mo  = $lr_yh{"max_mo"};
-  my $last_yr  = $lr_yh{"max_yr"};
-  my $last_key = $lr_yh{"max_key"};
-  my $last_summary = $lr_yh{"max_summary"};
+  $last_day = sprintf("%02d", $lr_yh{"max_day"});
+  $last_mo  = sprintf("%02d", $lr_yh{"max_mo"});
+  $last_yr  = $lr_yh{"max_yr"};
+  $last_key = $lr_yh{"max_key"};
+  $last_summary = $lr_yh{"max_summary"};
 
   print "Last report on $last_mo/$last_day/$last_yr: $last_key - $last_summary\n" unless $quiet;
 
@@ -218,12 +153,16 @@ if ( -e "./jira_get_last_report_date.pl") {
   }
 
   if ($dderr) {
-    print "Do you want to proceed (y/n)?";
-    $yorn = <>;
-    chomp($yorn);
-    if ($yorn ne "y") {
-      print "You didn't type \"y\" ... exiting\n";
-      exit;
+    if ($yes) {
+      print "y option given, proceeding\n";
+    } else {
+      print "Do you want to proceed (y/n)?";
+      $yorn = <>;
+      chomp($yorn);
+      if ($yorn ne "y") {
+	print "You didn't type \"y\" ... exiting\n";
+	exit;
+      }
     }
   }
 
@@ -233,246 +172,108 @@ if ( -e "./jira_get_last_report_date.pl") {
 
 }
 
-my $feed;
-if ($rss_file) {
-  $feed = XML::Feed->parse($rss_file);
-} else {
-  $feed = XML::Feed->parse(URI->new($url));  # This cannot be given a $url directly
+my $updatedDateStr = "$last_yr-$last_mo-$last_day";
+
+if ($max_days > 0) {
+  # this should probably an "AND"
+  $updatedDateStr = "-" . $max_days . "d";
+
 }
 
-# print Dumper \$feed;
+my $jql = "(project in (projectsLeadByUser(ltharris), projectsLeadByUser(jjtoth), projectsLeadByUser(mtmartin)) OR assignee in (ltharris, jjtoth, mtmartin)) and updatedDate > '$updatedDateStr' AND status != Archived ORDER BY updatedDate ASC";
 
-my @entries = $feed->entries;
+print "JQL:$jql\n" if $debug;
 
-my $jca_url = "https://$jira_domain/";
-my $jira = JIRA::Client::Automated->new($jca_url, $username, $password);
+my @entries;
+# my @entries = $jira2->issues({ jql => $jql });
+my $search = $jira2->iterator({ jql => $jql }, 1);
+if ($search->issue_count) {
+  while (my $issue = $search->next) {
+    push (@entries, $issue);
+  }
+}
+
 
 my %items;
-my %actions;
-my $ignored;
 my %done;
-my %resolutions;
-my %doneby;
-my %subtasks;
-my %issubtask;
-my %titles;
-my %keys;
-my %parents;
 
-# print "\nTODAY: M/D/Y: $mon/$mday/$year\n";
 my $range = " to $mon/$mday/$year\n";
 my $last_date = "$mon/$mday/$year";
 
 foreach my $entry (@entries) {
 
-  print "\n######\n" if $show_entries;
-  print Dumper \$entry  if $show_entries;
+  print "\n######\n" if $debug;
+  print Dumper \$entry  if $debug;
+  print "\n######\n" if $debug;
 
-  my $content = $entry->content;
-  my $title = $entry->title;
-  my $id = $entry->id;
-  my $issued = $entry->issued;
-  my @links = $entry->link;  # doesn't work as advertised
+  my $key = $entry->{"data"}{"key"};
+  my $title = $entry->{"data"}{"fields"}{"summary"};
+  my $status = $entry->{"data"}{"fields"}{"status"}{"name"};
+  # displayname instead of key or name so we don't have to change staff in the yaml
+  my $assignee = $entry->{"data"}{"fields"}{"assignee"}{"displayName"} || $entry->{"data"}{"fields"}{"assignee"}{"key"}; 
+  my $reporter = $entry->{"data"}{"fields"}{"reporter"}{"displayName"} || $entry->{"data"}{"fields"}{"reporter"}{"key"};
+  my $initials;
 
-   if ($show_entries) {
+  if (!$assignee) { $assignee = $reporter;}
 
-     my $summary = $entry->summary;
-     print Dumper \$content;
-     print Dumper \$title;
-     print Dumper \$summary;
-     print Dumper \$issued;
-     print Dumper \@links;
-
-   }
-
-  my $item_year = $issued->year();
-  my $item_month = $issued->month();
-  my $item_month_n = $issued->month_name();
-  my $item_month_a = $issued->month_abbr();
-  my $item_day = sprintf("%02d", $issued->day());
-
-  print "\nM/D/Y: $item_month_a/$item_day/$item_year\n"  if $show_entries;
-
-  my $Dd = Delta_Days($item_year, $item_month, $item_day,$year, $mon, $mday);
-
-  print "\tDelta Days: $Dd\n"  if $show_entries;
-
-  if ($max_days > 0) {
-    if ($Dd > $max_days) {
-      print "DD gt MXD == $Dd gt $max_days\n";
-      $range = "Includes items from $last_date" . $range;
-      last;
-    }
-  }
-
-  $last_date = "$item_month/$item_day/$item_year";
-
-  my $body = $content->body || "";
-  my $action;
-  my $resolution;
-  my $by;
-  my $key;
-  #   print Dumper \$body;
-
-  if ($show_entries) {
-
-    print "TITLE: \"$title\"\n";
-    print "CONTENT: $content\n";
-    if ($body eq "") {
-      print "NO BODY\n";
-    } else {
-      print "BODY: \"$body\"\n";
-    }
-  }
-
-  $title = $hs->parse($title);
-  # $content = $hs->parse($content);
-
-  if ($title =~ m/Weekly Report/) {
-
-    $ignored++;
-    print "is weekly report...Ignored:$ignored,Ignorelevel: $ignore_level\n";
-    if ($ignored >= $ignore_level) {
-      $range = "Includes items from $last_date" . $range;
-      # last;
-      $last_item = 1;
-    }
-
-  }
-
-  # we don't care about linked issues
-  next if ($title =~ m/linked \d* issues/);
-  next if ($title =~ m/removed the Link between/);
-
-  if ($body) {
-    $body = $hs->parse($body);
+  if ($staff{$assignee}) {
+    $initials = $staff{$assignee};
+  } elsif ($assignee) {
+    $initials = $assignee;
+  } elsif ($staff{$reporter}) {
+    $initials = $staff{$reporter};
+  } elsif ($reporter) {
+    $initials = $reporter;
   } else {
-    $body = "";
+    $initials = "PROBLEM WITH $key";
   }
 
-  $body =~ s/^\s+|\s+$//g;
-  $title =~ s/^\s+|\s+$//g;
-  $title =~ s/\n//g;
-
-  if ($show_entries) {
-
-    print "STRIPPED TITLE: $title\n";
-    print "STRIPPED CONTENT: $content\n";
-    print "STRIPPED BODY: $body\n";
-
-  }
-
-  # Title seems to be in the form of <NAME><ACTION>     <ACTIVTY TITLE>
-
-  ($title, $by, $action, $resolution, $key) = munge_title($title);
-
-  if (!$key) {
-
-    print "No key found for $title, skipping\n";
-    next;
+  if ($debug) {
+    print "####\n";
+    print Dumper \$entry;
+    print "KEY: '$key'\n";
+    print "TITLE: '$title'\n";
+    print "STATUS: '$status'\n";
+    print "ASSIGNEE: '$assignee'\n";
+    print "REPORTER: '$reporter'\n";
+    print Dumper \%staff;
+    print "STAFF[ASSIGNEE]: '" . $staff{$assignee} . "'\n";
+    print "INITIALS: '$initials'\n";
+    print "####\n";
 
   }
 
-  if (!defined($done{$title})) {
-    $done{$title} = 0;
-  }
+  if ($status =~ /[Dd]one/) {
 
-  if ($resolution eq "Done") {
+    $done{$key} = "$key - $title [$initials]";
 
-    $done{$title} = 1;
-
-  } elsif ($done{$title} < 1) {
-
-    $done{$title} = 0;
-
-  }
-
-  if ($show_entries) {
-    if ($debug) {
-      print "TITLE: '$title'\n";
-      print "\t$body\n" unless $body eq "";
-      print "\tRESOLUTION: $resolution\n" unless $resolution eq "";
-      print "\tBY: $by\n" unless $by eq "";
-      print "\tKEY: $key\n";
-      #  print "BODY: '$body'\n";
-    }
-  }
-
-  if ($short_items) {
-    my $sentences=get_sentences($body);
-    $body = @$sentences[0];
-  }
-
-  print "BODY AFTER GET SEN: '$body'\n"  if $show_entries;
-
-  # we don't want forwarded emails...
-  if (($body) && ($body =~ m/---------- Forwarded message ----------/))  {
-    $body = "";
-  }
-
-  ## now to figure out if it is a subtask or has subtasks.
-  if (!$titles{$key}) {
-    # we haven't seen this title before, so it needs to be checked.
-
-    $titles{$key} = $title;
-    $keys{$title} = $key;
-    # print "key => titles: $key => " . $titles{$key} . "\n";
-    my $parent = getparent($key);
-
-    # problem  if we haven't seen the parent yet
-    if (!$titles{$parent}) {
-      $titles{$parent} = gettitle($parent);
-    }
-
-    # print "title:$title, key:$key, parent:$parent\n";
-    # print "title of parent: " . $titles{$parent} . "\n";
-
-    if (($parent) && !($parent =~ m/No/))  {
-      $issubtask{$title} = 1;
-      $parents{$title} = $parent;
-      push(@{$subtasks{$titles{$parent}}}, $title);
-      # push(@{$subtasks{$titles{$key}}}, $title);
-    }
-
-  }
-
-  $body =~ s/\n//g;
-  $body =~ s/^\s+|\s+$//g;
-  # print "BODY:'$body'\n";
-
-  if ($body) {
-    $items{$title} .= "\n\t$body";
   } else {
-    $items{$title} .= "";
+
+    $items{$key} = "$key - $title [$initials]";
+
   }
 
-  $actions{$title} .= $action;
-  $resolutions{$title} .= $resolution;
-  # push(@{$doneby{$title}}, $by);
-  $doneby{$title}{$by}++;
-
-  if ($last_item) { last; }
-  ## TODO: Move Done Tasks to Archived
 }
 
-# print Dumper \$doneby{'AUD-7 - Create IT Strategy Roadmaps'};
-# exit
-# print Dumper \%doneby;
-# exit;
-# print Dumper \%done;
+  if ($debug) {
+    print "### DONE ###\n";
+    print Dumper \%done;
+    print "### ITEMS ###\n";
+    print Dumper \%items;
+    print "###\n";
+#    exit;
+  }
 
 if (!($range =~ m/Includes/)) {
   $range = "Includes items from $last_date" . $range;
 }
 
-
 ### THE ACTUAL REPORT ####
 
 print "\n\n$range";
 
-# print_report("Heading", Done_Val, %items_or_done_hash, group_by_parent_task)
-print_report("JIRA DONE TASKS", 1, \%done, 0);
-print_report("JIRA ACTIVE TASKS", 0, \%items, 0);
+print_report("JIRA DONE TASKS", \%done);
+print_report("JIRA ACTIVE TASKS", \%items);
 
 if ((!$no_done) && (!$dry)) {
 
@@ -488,21 +289,10 @@ sub export_done {
 
   open (DONE, "+>/tmp/done.txt");
 
-  foreach my $t (keys %keys) {
-    # print "Checking: $t\n";
-    # print "TITLES(k): " . $titles{$k} . "\n";
-    # print "DONE(TITLES(k)): " . $done{$titles{$k}} . "\n";
-    #   print "KEYS(t): " . $keys{$t} . "\n";
-    # print "DONE(t): " . $done{$t} . "\n";
-    if ($done{$t} == 1) {
-      print DONE $keys{$t} . "\n";
-    }
+  foreach my $t (keys %done) {
+    print DONE $t . "\n";
 
   }
-
-#   print Dumper \%done;
-#   print Dumper \%keys;
-#   print Dumper \%titles;
 
   close(DONE);
 
@@ -513,206 +303,15 @@ sub export_done {
 sub print_report {
 
   my $heading = shift;
-  my $done_val = shift;
   my $tasks = shift;
-  my $group = shift;  # TODO: actually use this
 
-  # print "HEADING: $heading\n";
-  # print "DONEVAL: $done_val\n";
-  # print Dumper \$tasks;
-
-  # print "SUBTASKS: \n";
-  # print Dumper \%subtasks;
-
-  # print "\n##########\n";
   print $heading . "\n";
-  # print "Grouping: $group\n";
+
   foreach my $title (nsort keys $tasks) {
 
-    next unless $done{$title} == $done_val;
-
-    # giving up on grouping by parent for now.
-    if ($group == 1) {
-      print "grouping by parent not supported yet";
-    } else {
-      # don't group by parent tasks
-
-      print $title;
-      print_doneby($title);
-      if ($issubtask{$title}) {
-	print " (subtask of " . $parents{$title} . ")";
-      }
-      print_items($title);
-
-    }
+    print $tasks->{$title} . "\n";
 
   }
-
-}
-
-sub print_items {
-
-  my $title = shift;
-
-  if (!$no_items) {
-    print "\t" . $items{$title} . "\n" unless !$items{$title};
-  } else {
-    print "\n";
-  }
-
-}
-
-sub print_doneby {
-
-  my $title = shift;
-
-  print " [";
-  print join(",", sort keys %{$doneby{$title}});
-  print "]";
-
-}
-
-
-# print "\n########\n";
-# print Dumper \%actions;
-
-sub munge_title {
-
-  my $title = shift;
-
-  print "##Munging Title: '$title'##\n" if $debug;
-
-  chomp($title);
-
-  # who
-
-  my $regex = "^(";
-  $regex .= join "|", keys(%staff);
-  $regex .= ")";
-
-  # print "REGEX: $regex\n";
-
-  $title =~ s/$regex//;
-
-  print "NEW TITLE(1): $title\n" if $debug;
-  my $staff1 = $1;
-  print "DOLLAR1: \"$staff1\", STAFF: " . $staff{$staff1} . "\n" if (($debug) && ($staff1 ne ""));
-
-  if (!$staff1) {
-    # panic and try to guess the staff
-
-    my $verbs_regex = "(";
-    $verbs_regex .= join "|", @verbs;
-    $verbs_regex .= ")";
-    print "VERBS REGEX: $verbs_regex\n" if $debug;
-    $title =~ s/^(.*) $verbs_regex/$2/;
-    $staff1 = $1;
-    print "GUESS FOR STAFF1: $staff1\n" if $debug;
-  }
-
-  if (!$staff{$staff1}) {
-    print "unknown staff: $staff1\n";
-    $staff{$staff1} = $staff1;
-  }
-
-  print "GUESS FOR STAFF{STAFF1}:" . $staff{$staff1} . "\n" if $debug;
-  print "NEW TITLE(2): $title\n" if $debug;
-
-  # action
-
-  $title =~ s/(.*?\s{2,})//;
-  my $action = $1;
-  if ($action =~ m/changed the Summary of/) {
-    # need to remove the first 'to' from the title
-    print "Summary change\n" if $debug;
-    $title =~ s/([A-Z]*-[0-9]*)\s*to.*?'(.*)'/$1 - $2/;
-    print "New Title (Summary Change): $title\n" if $debug;
-  }
-
-  $title =~ s/with a resolution of '(.*)'//;
-  my $resolution = $1;
-
-  # for simplity we're going to consider a status change a resolution
-  if (($resolution) && ($resolution =~ m/changed the status to (.*) on/)) {
-    # print "Status change not resolution\n" if $debug;
-    $resolution = $1;
-  }
-
-  ## if $resolution is still null at this point, make it equal to action
-  if (!$resolution) { $resolution = $action; } # not the best idea but it works
-
-  my $timespent;
-  if ($action =~ m/Time Spent/) {
-
-    $title =~ s/(by '[0-9]* day')//;
-    $timespent = $1;
-
-  }
-
-  # remove trailing whitespace
-
-  $title =~ s/\s*$//;
-
-  my ($key) = $title =~ m/([A-Z0-9]+-[0-9]+)/;
-
-  print "##Returning Munged Title##\n\n" if $debug;
-  print "\tTitle: $title\n" if $debug;
-  print "\tSTAFF1: $staff1\n" if $debug;
-  print "\tRESOLUTION: $resolution\n" if $debug;
-  print "\tKEY: $key\n" if $debug;
-
-  return ($title,$staff{$staff1},$action,$resolution,$key);
-
-}
-
-sub gettitle {
-
-  my $t;
-  my $k = shift;
-  my $jql = "issue = $k";
-
-  my @results = eval{ $jira->all_search_results($jql, 1) } ;
-
-  if (@results) {
-
-    $t = $results[0] ->{"fields"}->{"summary"};
-    $t = $k . " - " . $t;
-
-  } else {
-
-    # print "NO RESULT FOR: $jql\n";
-
-  }
-
-  return $t;
-
-}
-
-
-sub getparent {
-
-  my $parent;
-  my $k = shift;
-
-  # print "Getting Parent for $k\n";
-
-  my $jql = "issue = $k";
-
-  my @results = eval{ $jira->all_search_results($jql, 1) } ;
-
-  if (@results) {
-
-    $parent = $results[0]->{"fields"}->{"parent"}->{"key"} || "No Parent";
-
-  } else {
-
-    $parent = "No Result";
-
-  }
-
-  # print "\tParent: $parent\n";
-
-  return $parent;
 
 }
 
@@ -733,7 +332,7 @@ sub ck_exit_status {
 
 sub usage {
 
-  print "Usage: $0 [--days=x] [--ignore_weekly=x] [--no-tiems] [--short-items] [--no-export-done] [--use-rss-file=/path/to/filename.rss] [--write-rss]\n";
+  print "Usage: $0 [--days=x] [--no-export-done] \n";
   exit;
 
 }
